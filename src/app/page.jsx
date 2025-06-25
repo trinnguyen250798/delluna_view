d9877cbf7da980cf840a0ae22e2652c4729a8a08
Dinhtri <nguyendinhtrijob@gmail.com>
2025-06-25 21:10:08 +0700

    a

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,24 +14,10 @@ export default function Page() {
                     id="dropdownButton"
                     data-dropdown-toggle="dropdownMenu"
                     className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-                    type="button"
-                >
-                    Dropdown
-                    <svg
-                        className="w-4 h-4 ml-2"
-                        aria-hidden="true"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                    >
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-                    </svg>
+                    type="button"> Dropdown
+                    <svg className="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" /></svg>
                 </button>
-
-                <div
-                    id="dropdownMenu"
-                    className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44"
-                >
+                <div id="dropdownMenu" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
                     <ul className="py-2 text-sm text-gray-700">
                         <li>
                             <a href="#" className="block px-4 py-2 hover:bg-gray-100">
